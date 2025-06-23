e1fb669a325ebd3e37539a9958cd7ff6971feeb1
ozdemirilayda <ozdemirilayda95@gmail.com>
2025-06-23 11:27:43 +0300

    Add z-index

--- storebrand/src/app/components/Navigation.tsx
+++ storebrand/src/app/components/Navigation.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navigation() {
   return (
-    <nav className="fixed top-0 left-0 right-0 bg-[#003d5d] shadow-md py-4">
+    <nav className="fixed top-0 z-50 left-0 right-0 bg-[#003d5d] shadow-md py-4">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex justify-between items-center">
           <Link href="/" className="flex items-center">
