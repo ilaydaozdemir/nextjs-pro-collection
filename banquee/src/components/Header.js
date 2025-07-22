4f702525f6517dd728dbb167bafb392037439af7
ozdemirilayda <ozdemirilayda95@gmail.com>
2025-07-23 00:13:47 +0300

    Add sidebar condition

--- banquee/src/components/Header.js
+++ banquee/src/components/Header.js
@@ -3,60 +3,78 @@ import { FaAngleDown } from "react-icons/fa";
 import styles from "./Header.module.scss";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import Sidebar from "./Sidebar";
 
 const Header = () => {
   const pathName = usePathname();
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkScreen = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    checkScreen();
+    window.addEventListener("resize", checkScreen);
+    return () => window.removeEventListener("resize", checkScreen);
+  }, []);
   return (
     <div className={styles.headerContainer}>
-      <div className={styles.container}>
-        <div className={styles.navLeft}>banquee</div>
-        <div className={styles.navCenter}>
-          <Link href="/features">
-            <span
-              className={
-                pathName === "/features" ? styles.active : styles.navitem
-              }
-            >
-              Features
-            </span>{" "}
-          </Link>
-          <Link href="/compare">
-            <span
-              className={
-                pathName === "/compare" ? styles.active : styles.navitem
-              }
-            >
-              Compare
-              <span className={styles.icon}>
-                <FaAngleDown color="#1A191E" />
+      {isMobile ? (
+        <Sidebar />
+      ) : (
+        <div className={styles.container}>
+          <div className={styles.navLeft}>banquee.</div>
+          <div className={styles.navCenter}>
+            <Link href="/features">
+              <span
+                className={
+                  pathName === "/features" ? styles.active : styles.navitem
+                }
+              >
+                Features
+              </span>{" "}
+            </Link>
+            <Link href="/compare">
+              <span
+                className={
+                  pathName === "/compare" ? styles.active : styles.navitem
+                }
+              >
+                Compare
+                <span className={styles.icon}>
+                  <FaAngleDown color="#1A191E" />
+                </span>
               </span>
-            </span>
-          </Link>
-          <Link href="/support">
-            <span
-              className={
-                pathName === "/support" ? styles.active : styles.navitem
-              }
-            >
-              Support
-            </span>
-          </Link>
-          <Link href="/blog">
-            <span
-              className={pathName === "/blog" ? styles.active : styles.navitem}
-            >
-              Blog
-              <span className={styles.icon}>
-                <FaAngleDown color="#1A191E" />
+            </Link>
+            <Link href="/support">
+              <span
+                className={
+                  pathName === "/support" ? styles.active : styles.navitem
+                }
+              >
+                Support
               </span>
-            </span>
-          </Link>
+            </Link>
+            <Link href="/blog">
+              <span
+                className={
+                  pathName === "/blog" ? styles.active : styles.navitem
+                }
+              >
+                Blog
+                <span className={styles.icon}>
+                  <FaAngleDown color="#1A191E" />
+                </span>
+              </span>
+            </Link>
+          </div>
+          <div className={styles.navRight}>
+            <button className={styles.login}>Login</button>
+            <button className={styles.openAccount}>Open Account</button>
+          </div>
         </div>
-        <div className={styles.navRight}>
-          <button className={styles.login}>Login</button>
-          <button className={styles.openAccount}>Open Account</button>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
